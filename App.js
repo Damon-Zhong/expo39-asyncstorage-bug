86d8f7b7b7188b8b2460539ffd6d1b414613bb1c
damon <damon.yq.zhong@gmail.com>
2020-12-02 13:55:21 -0500

    repro react-native-async-storage-bug: after eject, project runs on emulator but stuck on splash screen if run expo publish

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
+  useEffect(() => {
+    loadStorage();
+  }, []);
+
+  const loadStorage = async () => {
+    const result = await AsyncStorage.setItem(
+      "currentUser",
+      JSON.stringify({ userNumber: "123" })
+    );
+    console.log("async storage", result);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
